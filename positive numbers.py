de239bb6254a136e1f610cd7c73d332ebc4a1852
ARYAGALA <109096150+ARYAGALA@users.noreply.github.com>
2022-07-24 10:53:48 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,29 @@
+# print positive Numbers in a List
+  
+# input of list
+my_list = [12,-7,5,64,-14]
+print("Positive numbers in",my_list,"are: ")
+  
+# using list comprehension
+positive_num = [num for num in my_list if num >= 0]
+  
+print(positive_num)
+
+
+
+
+# print positive Numbers in a List
+  
+# input of list
+my_list1 = [12,14,-95,3]
+print("Positive numbers in",my_list1,"are: ")
+  
+# using list comprehension
+positive_num = [num for num in my_list1 if num >= 0]
+  
+print(positive_num)
+
+
+
+
+
